b1ba53b953c5b82fb5c35439dd7491db6d7dc422
benfran21 <benfran21@hotmail.com>
2021-05-25 11:51:44 -0400

    se agrego codigo para llamado de favicon y link de rrss

--- maintenance.php
+++ maintenance.php
@@ -7,7 +7,24 @@
     <meta name="description" content="WOOW Colombia">
     <meta name="keywords" content="Cuidado de tus mascotas, Rescate animal, Busqueda por GPS">
     <meta name="author" content="Francisco González">
-    <link rel="icon" href="#" type="image/png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="./wp-content/plugins/um_vdbw404/img/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="./wp-content/plugins/um_vdbw404/img/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="./wp-content/plugins/um_vdbw404/img/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./wp-content/plugins/um_vdbw404/img/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="./wp-content/plugins/um_vdbw404/img/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="./wp-content/plugins/um_vdbw404/img/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="./wp-content/plugins/um_vdbw404/img/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="./wp-content/plugins/um_vdbw404/img/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./wp-content/plugins/um_vdbw404/img/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="./wp-content/plugins/um_vdbw404/img/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./wp-content/plugins/um_vdbw404/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./wp-content/plugins/um_vdbw404/img/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./wp-content/plugins/um_vdbw404/img/favicon-16x16.png">
+    <link rel="manifest" href="./wp-content/plugins/um_vdbw404/img/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="./wp-content/plugins/um_vdbw404/img/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    
     <title>EN CONSTRUCCIÓN
     </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -22,10 +39,10 @@
         <h2>ESTÁ POR LLEGAR</h2>
         <h5>ESPERALO EN</h5>
         <h4><span id="dias">25</span> DÍAS</h4>
-        <a href="#">
+        <a class="rrssLink" target="_blank" href="https://www.instagram.com/woowcolombia/">
             <img class="img-thumbnail" src="./wp-content/plugins/um_vdbw404/img/iconoInstagram.svg" alt="Instagram">
         </a>
-        <a href="#">
+        <a class="rrssLink" target="_blank" href="https://www.facebook.com/WoowColombia">
             <img class="img-thumbnail" src="./wp-content/plugins/um_vdbw404/img/iconoFacebook.svg" alt="Facebook">
         </a>
     </div>
